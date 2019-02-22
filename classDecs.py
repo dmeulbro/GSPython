d0411b718c512ebf9722dfddbb611f6912bfa76a
Daniel Meulbroek <dmeulbro@purdue.edu>
2019-02-22 12:07:13 -0500

    Moved classDecs stuff to approprate file and started official main function

--- classDecs.py
+++ classDecs.py
@@ -1,50 +1,77 @@
 import array
+import sys
+import random
 
 class Student:
-    def __init__(myStudent, studID, rList, paired, pProject, currRank):
+    def __init__(myStudent, studID, rList, paired, pProject, currRank):#, rList, paired, pProject, currRank):
         myStudent.studID = studID
-        myStudent.rList = array.array('b')
+        myStudent.rList = rList
         myStudent.paired = paired
         myStudent.pProject = pProject
         myStudent.currRank = currRank
 
-    def freeStudent(myStudent):
-        myStudent.paired = false
-        myStudent.pProject = void
-        myStudent.rList.pop
-
 class Project:
-    def __init__(myProject, projNum, pList, numMems, maxSize, memList):
+   def __init__(myProject, projNum, pList, numMems, maxSize, memList):
         myProject.projNum = projNum
-        myProject.pList = array.array('i')
+        myProject.pList = pList
         myProject.numMems =numMems
         myProject.maxSize = maxSize
-        myProject.memList = #learn to do two dimensional array
+        myProject.memList = memList
 
-    def freeProject(myProject):
-        myProject.numMems -= 1
-        myProject.memList.pop
+# generate project list with all parameters
+def genProjects(numStudents, numProjects):
+    projList = []
+    for i in range(0,numProjects):
+        pList = genRankedlist(numProjects)
+        projList.append(Project(i+1, pList, None, (int(numStudents)/int(numProjects)), None))
+    printProjects(projList)
 
-def checkPlist(student, project):
-    for i in range(0, sizeof(project.memList
+# generate student list with all parameters
+def genStudents(numStudents, numProjects):
+    studList = []
+    for i in range(0,numStudents):
+        rList = genRankedlist(numProjects);
+        studList.append(Student(i+1, rList, 0, None, None))
+#    printStudents(studList)
+
+# generate ranked list for students
+def checkNum(num, projList):
+    flag = 0
+    for i in range (0,len(projList)):
+        if(projList[i] == num):
+            flag = 1
+    return flag
+
+def genRankedlist(numProjects):
+    projList = []
+    while(len(projList) < numProjects):
+        newNum = random.randint(1,numProjects)
+        if(checkNum(newNum, projList) == 0):
+            projList.append(newNum);
+    return projList
+
+#test functions
+def printStudents(studList):
+    for i in range(0,len(studList)):
+        print studList[i].studID
+        print studList[i].rList
+        print studList[i].paired
+        print studList[i].pProject
+        print studList[i].currRank
+        print "\n"
 
 
-def pushStud(student, project, studList):
-    studRank = getRank(student, project)
-    if(sizeof(project.memList) == 0):
-        project.memList.append(student)
-        student.currRank = studRank
-    else
-        for i in range(0,sizeof(project.memList)):
-            if(studRank > project.memList[i].currRank):
-                project.memList.insert(i, student)
-                break
-return
-                
-                
+def printProjects(projList):
+    for i in range(0,len(projList)):
+        print projList[i].projNum
+        print projList[i].pList
+        print projList[i].numMems
+        print projList[i].maxSize
+        print projList[i].memList
+        print "\n"
 
-def popStud(studList, project)
 
-def getRank(student, project)
+#Test function calls
+#genStudents(50, 10)
+#genProjects(50, 10)
 
-#still need to write checkPlist, popStud, getRank, and functions to generate students and projects
--- main.py
+++ main.py
@@ -1,77 +1,8 @@
-import array
-import sys
-import random
 
-class Student:
-    def __init__(myStudent, studID, rList, paired, pProject, currRank):#, rList, paired, pProject, currRank):
-        myStudent.studID = studID
-        myStudent.rList = rList
-        myStudent.paired = paired
-        myStudent.pProject = pProject
-        myStudent.currRank = currRank
-
-class Project:
-   def __init__(myProject, projNum, pList, numMems, maxSize, memList):
-        myProject.projNum = projNum
-        myProject.pList = pList
-        myProject.numMems =numMems
-        myProject.maxSize = maxSize
-        myProject.memList = memList
-
-# generate project list with all parameters
-def genProjects(numStudents, numProjects):
-    projList = []
-    for i in range(0,numProjects):
-        pList = genRankedlist(numProjects)
-        projList.append(Project(i+1, pList, None, (int(numStudents)/int(numProjects)), None))
-    printProjects(projList)
-
-# generate student list with all parameters
-def genStudents(numStudents, numProjects):
-    studList = []
-    for i in range(0,numStudents):
-        rList = genRankedlist(numProjects);
-        studList.append(Student(i+1, rList, 0, None, None))
-#    printStudents(studList)
-
-# generate ranked list for students
-def checkNum(num, projList):
-    flag = 0
-    for i in range (0,len(projList)):
-        if(projList[i] == num):
-            flag = 1
-    return flag
-
-def genRankedlist(numProjects):
-    projList = []
-    while(len(projList) < numProjects):
-        newNum = random.randint(1,numProjects)
-        if(checkNum(newNum, projList) == 0):
-            projList.append(newNum);
-    return projList
-
-#test functions
-def printStudents(studList):
-    for i in range(0,len(studList)):
-        print studList[i].studID
-        print studList[i].rList
-        print studList[i].paired
-        print studList[i].pProject
-        print studList[i].currRank
-        print "\n"
+def main():
+    genProjects(50,10)
 
 
-def printProjects(projList):
-    for i in range(0,len(projList)):
-        print projList[i].projNum
-        print projList[i].pList
-        print projList[i].numMems
-        print projList[i].maxSize
-        print projList[i].memList
-        print "\n"
 
-
-#Test function calls
-#genStudents(50, 10)
-#genProjects(50, 10)
-
+if __name__=='__main__':
+    main()
