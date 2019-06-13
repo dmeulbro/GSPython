24bd7a397d079f54568e38da5acf9044a039b066
Daniel Meulbroek <dmeulbro@purdue.edu>
2019-06-13 09:30:49 -0400

    Made copies of student list. Going to shuffle and put thought alg to see if output is the same

--- main.py
+++ main.py
@@ -12,54 +12,51 @@ def main():
     percList    = []
     studList_ref = [None] * 10
     projList_ref = [None] * 10
-#    while j < 1000:
+
     projList    = classDecs.genProjects(50,10)
     studList    = classDecs.genStudents(50,10)
-    while j < 2:
-        studList_ref[j] = studList.copy()
-        projList_ref[j] = projList.copy()
-#    print str(studList_ref
+    while j < 10:
+        print(j)
+        studList_ref[j] = copy.deepcopy(studList)
+        projList_ref[j] = copy.deepcopy(projList)
+        j += 1 
 
-    
-#    while j< 10:
-#        studList_ref[j] = copy.deepcopy(studList);
-#        projList_ref[j] = copy.deepcopy(projList);
+    max_size = math.floor(50/10)
 
-##    for i in range(len(studList)):
-#        print str(studList[i].studID) + ":"
-#        print studList[i].rList
-#        print str(studListCpy[i].studID) + ":"
-#        print studListCpy[i].rList 
-#        print "\n"
-#
-#    max_size = math.floor(50/10)
-#    print max_size
-#
-#    j = 0
-#    projList_1 = GSMatch.matchProj(studList, projList, max_size)
-#    projList_2 = GSMatch.matchProj(studListCpy, projListCpy, max_size)
-#
-##    while j < 10:
-##        projList_ref[j] = GSMatch.matchProj(studList_ref[j], projList_ref[j], max_size)
-##
-##
+    j = 0
+    while j < 10:
+        projList_ref[j] = GSMatch.matchProj(studList_ref[j], projList_ref[j], max_size)
+        j += 1
+
+    for i in range(len(projList_ref)):
+        print ("\n")
+        print (j+1)
+        print
+        
 #    for i in range(len(projList_1)):
 #        proj = i+1
 #        # proj 1
-#        print "\n"
-#        print proj
-#        print "=-=-=-=-=-=-=-=-="
+#        print ("\n")
+#        print (proj)
+#        print ("=-=-=-=-=-=-=-=-=")
 #        for j in range(len(projList_1[i].memList)):
-#           print projList_1[i].memList[j].studID
-#        print "=-=-=-=-=-=-=-=-="
+#           print (projList_1[i].memList[j].studID)
+#        print ("=-=-=-=-=-=-=-=-=")
 #        # proj 2
-#        print "\n"
-#        print proj
-#        print "=-=-=-=-=-=-=-=-="
+#        print ("\n")
+#        print (proj)
+#        print ("=-=-=-=-=-=-=-=-=")
 #        for j in range(len(projList_2[i].memList)):
-#           print projList_2[i].memList[j].studID
-#        print "=-=-=-=-=-=-=-=-="
-#
+#           print (projList_2[i].memList[j].studID)
+#        print ("=-=-=-=-=-=-=-=-=")
+#        # proj 3
+#        print ("\n")
+#        print (proj)
+#        print ("=-=-=-=-=-=-=-=-=")
+#        for j in range(len(projList_3[i].memList)):
+#           print (projList_3[i].memList[j].studID)
+#        print ("=-=-=-=-=-=-=-=-=")
+ 
 #for i in range(len(projList_2)):
 #        proj = i+1
 #        print "\n"
