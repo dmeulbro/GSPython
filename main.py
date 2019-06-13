0b69d6ba569f8e8d532886bb82baacbc683cda87
Daniel Meulbroek <dmeulbro@purdue.edu>
2019-06-13 16:11:30 -0400

    several iterations of the same survey data to see if GS optimizes

--- main.py
+++ main.py
@@ -16,23 +16,39 @@ def main():
     projList    = classDecs.genProjects(50,10)
     studList    = classDecs.genStudents(50,10)
     while j < 10:
-        print(j)
         studList_ref[j] = copy.deepcopy(studList)
+        random.shuffle(studList_ref[j])
         projList_ref[j] = copy.deepcopy(projList)
         j += 1 
 
     max_size = math.floor(50/10)
-
+    teams = []
     j = 0
     while j < 10:
         projList_ref[j] = GSMatch.matchProj(studList_ref[j], projList_ref[j], max_size)
         j += 1
-
     for i in range(len(projList_ref)):
-        print ("\n")
-        print (j+1)
-        print
-        
+        print("=-=-=-=-= Iteration: " + str(i+1) + " =-=-=-=-=\n")
+        for k in range(len(projList_ref[i])):
+            print("Team: " + str(k+1))
+            temp = [None] * 5
+            print("=-=-=-=-=-=-=-=-=-=-=")
+            for t in range(len(projList_ref[i][k].memList)):
+                temp[t] = int(projList_ref[i][k].memList[t].studID)
+                print(int(projList_ref[i][k].memList[t].studID))
+            print("=-=-=-=-=-=-=-=-=-=-=\n")
+            teams.append(temp)
+    for i in range(len(teams)):
+        teams[i].sort()
+#    for i in range(len(projList_ref)):
+#        print ("\n")
+#        print (j+1)
+#        print ("=-=-=-=-=-=-=-=-=-=")
+#        for t in range(len(projList_ref[i])):
+#            projList_ref[i]
+#        print ("=-=-=-=-=-=-=-=-=-=")
+#        print ("\n")
+#        
 #    for i in range(len(projList_1)):
 #        proj = i+1
 #        # proj 1
@@ -71,10 +87,10 @@ def main():
 #    percList.append(analysis.percentMatch(countList[j], 70))
 #        j += 1
 
-#    with open('countdata7.csv', 'wb') as myfile:
-#        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-#        for i in range(len(countList)):
-#            wr.writerow(countList[i])
+    with open('teamdata.csv', 'w') as myfile:
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        for i in range(len(teams)):
+            wr.writerow(teams[i])
 #    with open('percdata7.csv', 'wb') as myfile:
 #        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
 #        for i in range(len(percList)):
